67714a1e655f1f7a833364f810a113cde1960f69
Msbee502 <b.hoogesteyn@gmail.com>
2024-11-15 08:55:10 +0100

    First commit

---
+++ practise.js
@@ -0,0 +1,63 @@
+const words = [
+    "dependent", "dog", "superficial", "admit", "juice",
+    "javascript", "developer", "airplane", "great", "fun",
+    "manipulate", "cat", "transition", "school", "computer",
+    "programming", "drag", "loving", "north", "access", "phantom", "vacation", "accommodate", "liaison",
+  ];
+
+  const timeContainer = document.getElementById("word");
+  const scoreContainer = document.getElementById("score");
+  const input = document.getElementById("text");
+  const endGameContainer = document.getElementById("end-game-container");
+  const wordContainer = document.getElementById("word");
+
+  //Variables
+  let time = 10; 
+  let score = 0; 
+  let timer;
+
+  //Function that selects a random word from the array
+  function addWordToDOM(){
+    //Random index
+    const randomWord = math.floor(math.random() * words.length);
+    //Select a word from the array using randomWord
+    const selectedWord = words[randomWord];
+    //Display selected word
+    wordContainer.innerText = selectedWord;
+
+}
+
+function updateScore(){
+    //Updates the score with 1
+     score ++;     
+    //Displays the score in  scorecontainer
+    scoreContainer.innerText = score;
+
+}
+
+// Add an event listener to the text element. When you type in the correct word, the function should: 
+//     • update score 
+//     • give the user a new word 
+//     • increment time by 5 seconds 
+//     • reset the input to empty string
+
+text.addEventListener("text", function(e){
+    
+
+    if (input === addWordToDOM){
+    updateScore()
+    addWordToDOM() 
+    input 
+    time= +5;
+    }
+})
+
+
+
+
+
+// const input = document.querySelector("input");
+
+// input.addEventListener("input", function (e) {
+//     console.log(this.value);
+// })
